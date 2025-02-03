0178e4d03e8498bd60d7ee5a45a09db36b31e534
skyturtle <dev@skyturtle.io>
2025-02-03 11:36:49 -0800

    add health check plug

--- lib/monolith_web/endpoint.ex
+++ lib/monolith_web/endpoint.ex
@@ -11,6 +11,11 @@ defmodule MonolithWeb.Endpoint do
     same_site: "Lax",
   ]
 
+  # Run custom plug for healthcheck first so that the
+  # healthcheck will run, then stop, without having
+  # to run the rest of the plugs.
+  plug MonolithWeb.HealthCheck
+
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
