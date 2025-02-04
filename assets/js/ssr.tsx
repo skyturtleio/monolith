ce06964e4f4c6bd0141522f0df2064fae1b09318
skyturtle <dev@skyturtle.io>
2025-02-03 23:00:39 -0800

    fix hydration by using default layout when server-side rendering
    I had a default layout setup for client-side rendering (CSR), but forgot
    to add when server-side rendering (SSR) React components

--- assets/js/ssr.tsx
+++ assets/js/ssr.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Layout from './layouts/Layout';
 import { createInertiaApp } from "@inertiajs/react";
 import { renderToString } from "react-dom/server";
+import { ReactNode } from 'react';
 
-export function render(page) {
+export function render(page: any) {
   return createInertiaApp({
     page,
     render: renderToString,
     resolve: async (name) => {
-      return await import(`./pages/${name}.tsx`);
+      const page = await import(`./pages/${name}.tsx`);
+      page.default.layout = page.default.layout || ((page: ReactNode) => <Layout children={page} />)
+      return page;
     },
     setup: ({ App, props }) => <App {...props} />,
   });
